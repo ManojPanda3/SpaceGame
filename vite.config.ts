4e10358c42b7e15b1ce4b4514ba2404d2ee14534
Manoj Panda <151429040+ManojPanda3@users.noreply.github.com>
2025-11-27 07:00:22 +0530

    Configure base URL for GitHub Pages
    Set the base URL for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/SpaceGame/',  // this is for github page so that i will use /SpaceGame url and not request assets from / route
     plugins: [
         tailwindcss(),
     ],
