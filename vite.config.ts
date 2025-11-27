77aed4394459b9e742baa1952c9c5519eb87056f
Manoj Panda <151429040+ManojPanda3@users.noreply.github.com>
2025-11-27 07:12:33 +0530

    Remove base URL for GitHub Pages
    Removed base URL configuration for GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/SpaceGame/',  // this is for github page so that i will use /SpaceGame url and not request assets from / route
     plugins: [
         tailwindcss(),
     ],
